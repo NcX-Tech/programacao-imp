7847629dd4796ced5b196143efc21df797e7de94
NcX-Tech <brenno.commercial@gmail.com>
2025-03-11 21:03:03 -0300

    commit Exemplo Api

---
+++ ExemploApi/ExemploApi.java
@@ -0,0 +1,11 @@
+package ExemploApi;
+
+public class ExemploApi {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i += 1) {
+            int aleatorio = (int) Math.floor(Math.random() * 6) + 1;
+            System.out.println(aleatorio);
+        }
+    }
+}
+
