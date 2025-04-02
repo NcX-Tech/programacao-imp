ffa8888502398d00226d3579fe73da32d0dab08a
NcX-Tech <brenno.commercial@gmail.com>
2025-04-01 21:07:26 -0300

    exercício finalizado

--- Exercício.java
+++ Exercício.java
@@ -1,16 +1,31 @@
-import java.util.Random;
+import java.util.Random; //importa o ramdom
 
-public class Exercício {
+public class Exercício { //define que a classe será publica
     public static void main(String[] args) {
-        int[] vetor = new int[1000]; 
-        Random random = new Random(); // Objeto para gerar números aleatórios
+        int[] vetor = new int[1000]; //define o comprimento do vetor
+        Random random = new Random(); // gerar números aleatórios
 
-        for (int i = 1; i < vetor.length; i++) {
+        for (int i = 1; i < vetor.length; i++) { 
             vetor[i] = random.nextInt(1000) + 1;
         }
-        System.out.println("Os primeiros 1000 números gerados são:");
+        
+
+        System.out.println("Os primeiros 1000 números gerados são:"); //gera o texto entre parenteses
         for (int i = 0; i < 1000; i++) {
             System.out.print(vetor[i] + " ");           
         }
     }
+
+        public static int busca(double[] v, int tam, double x) { 
+        for (int i = 0; i < tam; i++) {
+            if (v[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    
 }
+
+
