e7f9e9fbeb109d389537a1fe413abf8f5c88d639
NcX-Tech <brenno.commercial@gmail.com>
2025-04-01 21:25:51 -0300

    correções de identação e classes

--- Exercício.java
+++ Exercício.java
@@ -3,26 +3,31 @@ import java.util.Random; //importa o ramdom
 public class Exercício { //define que a classe será publica
     public static void main(String[] args) {
         int[] vetor = new int[1000]; //define o comprimento do vetor
+        preencher1(vetor);
+        imprimir(vetor);
+    }
+    
+    public static void preencher1(int[] v) {
         Random random = new Random(); // gerar números aleatórios
-
-        for (int i = 1; i < vetor.length; i++) { 
-            vetor[i] = random.nextInt(1000) + 1;
-        }
-        
-
-        System.out.println("Os primeiros 1000 números gerados são:"); //gera o texto entre parenteses
-        for (int i = 0; i < 1000; i++) {
-            System.out.print(vetor[i] + " ");           
+        for (int i = 1; i < v.length; i++) { 
+            v[i] = random.nextInt(1000) + 1;  
         }
     }
 
-        public static int busca(double[] v, int tam, double x) { 
+    public static void imprimir(int[] v) {
+        System.out.println("Os primeiros 1000 números gerados são:"); //gera o texto entre parenteses
+        for (int i = 0; i < 1000; i++) {
+            System.out.print(v[i] + " ");
+        }        
+    }
+
+    public static int busca(double[] v, int tam, double x) { 
         for (int i = 0; i < tam; i++) {
             if (v[i] == x) {
                 return i;
             }
         }
-        return -1;
+    return -1;
 
     }
     
