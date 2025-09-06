3126fa4c044a18c70e796ab5f61a1e99ee97fbcb
NcX-Tech <brenno.commercial@gmail.com>
2025-09-05 23:47:32 -0300

    ATIVIDADE DANIEL

--- DANIEL/LISTA1/MATRIZES/terceira.java
+++ DANIEL/LISTA1/MATRIZES/terceira.java
@@ -1,1 +1,16 @@
 package MATRIZES;
+
+public static void exercicio2() {
+    System.out.println("\n2. Matriz 4x4 com produto linha x coluna:");
+    int[][] matriz = new int[4][4];
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            matriz[i][j] = i * j;
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        System.out.println(Arrays.toString(matriz[i]));
+    }
+}
