204f70a1950c3c6bc51c2ab98865044ee3656062
NcX-Tech <brenno.commercial@gmail.com>
2026-04-03 20:50:11 -0300

    ATIVIDADES

---
+++ ATIVIDADES/atv1.java
@@ -0,0 +1,15 @@
+package ATIVIDADES;
+
+import java.util.Scanner;
+
+
+public class atv1 {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int[] A = new int[6];
+
+    A = {1, 0, 5, -2, -5, 7};
+    
+
+    }
+}
