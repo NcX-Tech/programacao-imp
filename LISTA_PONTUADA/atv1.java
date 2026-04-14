61a83f030aec2c47022233ea968e552e805e4151
NcX-Tech <brenno.commercial@gmail.com>
2026-04-14 19:25:52 -0300

    LISTA PONTUADA

---
+++ LISTA_PONTUADA/atv1.java
@@ -0,0 +1,22 @@
+package LISTA_PONTUADA;
+import java.util.Scanner;
+
+
+// Uma concessionaria registrou o consumo de energia eletrica de uma residencia ao longo de varios dias. 
+// Escreva um programa que leia um numero inteiro n, que representa a quantidade de dias analisados. 
+// Em seguida, leia n valores inteiros que representam o consumo de energia em cada dia, armazenados em um vetor.
+
+// O programa deve informar:
+
+// - o consumo total no periodo
+// - o consumo medio por dia
+// - a quantidade de dias em que o consumo foi maior que a media.
+
+public class atv1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int dias = 0;
+        intNTUADA
+    }
+}
